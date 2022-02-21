9f5b255a2aad4ad0ffd6adbed2e7c94ecd6641cb
Tusif Hassan Rizvi <tausifhassan123@gmail.com>
2022-02-21 21:41:15 +0530

    student name list

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project Title
+Student List
+
+# Project Description
+Student name list that show all the students name in a tabular form,\
+This small project made with HTML, CSS, & JavaScript.
+
+# Developer 
+Tusif Hassan Rizvi\
+tusifhassanrizvi@gmail.com
+
+# Screenshot
+
+![App screenshot](img/redme1.png)
+![App screenshot](img/redme2.png)
---
+++ script.js
@@ -0,0 +1,51 @@
+let pull = document.getElementById('pull');
+let hide = document.getElementById('hide');
+let table = document.getElementById('table');
+
+
+// student name list
+let students = ["Tusif", "Suhel", "Usama", "Saif", "Yusra", "Shivani", "Sahib", "Nisha", "Neeraj", "Vishal", "Kulsum", "Arif", "Zara", "Zuha", "Tafziluddin", "Nazia", "sumaiya", "Shilpa", "Shubham"];
+
+// show button logic
+pull.addEventListener('click', data);
+
+function data() {
+    pull.style.backgroundColor = 'green';
+    setTimeout(() => {
+        pull.style.backgroundColor = 'rgb(70, 70, 153)';
+    }, 100);
+
+ 
+    if (table.style.display == 'none') {
+        table.style.display = 'block';
+    }
+    else {
+        table.style.display = 'block';
+    }
+
+    let html = "";
+
+    for (let name of students) {
+        let index = students.indexOf(name);
+
+        html += `<li type="none">Name of (PGDCA 2021-22) student <b>${index + 1}</b> is: <b>${name}</b></li>`;
+
+    }
+    table.innerHTML = html;
+
+}
+
+// hide button logic
+hide.addEventListener('click', erase);
+function erase() {
+    hide.style.backgroundColor = 'green';
+    setTimeout(() => {
+        hide.style.backgroundColor = 'rgb(70, 70, 153)';
+    }, 100);
+
+    if (table.style.display == 'block') {
+
+        table.style.display = 'none';
+    }
+
+}
